f2b0a02b58add79837c4340981e66e4c43c96d2d
Micheal484 <johnmichealjohnjohn@gmail.com>
2025-08-07 10:17:13 +0800

    activity

---
+++ script.js
@@ -0,0 +1,18 @@
+const nameid = document.getElementById("name".Value);
+const usernameid = document.getElementById("usernam").Value;
+const passwordid = document.getElementById("password").Value;
+
+const form = document.getElementById("form");
+
+btnclick.addEventListener("btn-click", function() {
+    console.log("NameID: " + nameid);
+    console.log("UserName: " + usernameid);
+    console.log("Password: " + passwordid);
+});
+
+
+
+
+
+
+
